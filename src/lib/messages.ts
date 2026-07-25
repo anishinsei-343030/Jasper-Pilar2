c9c998f9c2af139a6ea1cced9346d0e062cc2e6d
Anishinsei <Anishinsei343030@gmail.com>
2026-07-25 22:40:40 +0800

    fix: replace text peek previews with 4-icons-per-box layered deck

--- src/lib/messages.ts
+++ src/lib/messages.ts
@@ -13,7 +13,7 @@ export const boxes = [
     tagline: 'Words I mean',
     emoji: '/Icons/MessageIcon.png',
     color: '#3B82F6',
-    peekPreviews: ['Some people come into your life...', 'You are one of those rare souls', '/Icons/MessageIcon.png', 'With all my love'],
+    peekPreviews: ['/Icons/MessageIcon1.png', '/Icons/MessageIcon2.png', '/Icons/MessageIcon3.png', '/Icons/MessageIcon.png'],
   },
   {
     id: 'wishes',
@@ -21,7 +21,7 @@ export const boxes = [
     tagline: 'For you, always',
     emoji: '/Icons/WishIcon.png',
     color: '#93C5FD',
-    peekPreviews: ['May every day bring you joy', 'Chase every dream that sets your soul on fire', '/Icons/WishIcon.png', 'Dreams come true'],
+    peekPreviews: ['/Icons/WishIcon1.png', '/Icons/WishIcon2.png', '/Icons/WishIcon3.png', '/Icons/WishIcon.png'],
   },
   {
     id: 'finale',
@@ -29,7 +29,7 @@ export const boxes = [
     tagline: 'A little surprise',
     emoji: '/Icons/GiftIcon.png',
     color: '#0F172A',
-    peekPreviews: ['Some people are unforgettable', 'Here\'s to you, always and forever', '/Icons/GiftIcon.png', 'Always for you'],
+    peekPreviews: ['/Icons/GiftIcon1.png', '/Icons/GiftIcon2.png', '/Icons/GiftIcon3.png', '/Icons/GiftIcon.png'],
   },
 ]
 
